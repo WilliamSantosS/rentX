2d05cbadbe9265a78761832bb66bafa5f9f88121
William santos <smurf455@hotmail.com>
2022-05-20 21:49:00 -0300

    using swagger to implement the api documentation

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import SwaggerUI from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerJson from "./swagger.json";
 
 const app = express();
 app.use(express.json());
 
+app.use("/api-docs", SwaggerUI.serve, SwaggerUI.setup(swaggerJson));
 app.use(router);
 
 app.listen(3333, () => console.log("The server is running"));
