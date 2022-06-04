f19eebccdf07e0a68fb4fdcba0c8bd3067781cb2
William Santos <smurf455@hotmail.com>
2022-06-03 22:31:18 -0300

    creating AppError class

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import "reflect-metadata";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import SwaggerUI from "swagger-ui-express";
 
-import { router } from "./routes";
-import swaggerJson from "./swagger.json";
 import "./shared/container";
 
+import { AppError } from "./errors/AppError";
+import { router } from "./routes";
+import swaggerJson from "./swagger.json";
 import "./database";
 
 const app = express();
@@ -13,5 +14,16 @@ app.use(express.json());
 
 app.use("/api-docs", SwaggerUI.serve, SwaggerUI.setup(swaggerJson));
 app.use(router);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({ message: error.message });
+    }
+    return response
+      .status(500)
+      .json({ message: `Internal Server Error: ${error.message}` });
+  }
+);
 
 app.listen(3333, () => console.log("The server is running"));
